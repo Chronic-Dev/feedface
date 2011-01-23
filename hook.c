e3f0a363bff77a7426896c65132564f502193de0
pod2g <pod2g@gmx.com>
2011-01-23 20:11:24 +0100

    no clue

--- hook.c
+++ hook.c
@@ -1,11 +1,8 @@
 #include "payload.h"
 #include "patching.h"
 
-#define ARM_BX_R12 0xe12fff1c
-#define THUMB_BX_R12 0x4760
-
 static void hook2() {
-	IOLog("hooked %08x %08x %08x\n");
+	//IOLog("in hook2.\n");
 }
 
 __attribute ((naked, flatten)) void hook() {
@@ -30,19 +27,33 @@ __attribute ((naked, flatten)) void hook() {
 	"mov r11, r7\n\t"
 	"pop {r0-r7}\n\t"
 	"add r5, #7\n\t"
+	//"add r5, #8\n\t"
 	"mov r12, r5\n\t"
 	"pop {r4}\n\t"
 	"mov lr, r4\n\t" // original lr restored
 	"pop {r4-r5}\n\t" // orginal r4,r5 restored
 	);
 
+	/*
+	// original sandox hooked function prolog (converted to thumb)
+	asm (
+	"push {r4-r7, lr}\n\t"
+	"add r7, sp, #0xc\n\t"
+	"mov r4, r8\n\t"
+	"mov r5, r10\n\t"
+	"mov r6, r11\n\t"
+	"push {r4-r6}\n\t"
+	"sub sp, #0xfc\n\t"
+	"mov r10, r0\n\t"
+	);
+	*/
 	// original hooked function prolog
 	asm (
 	"push {r0-r3}\n\t"
 	"push {r4-r7,lr}\n\t"
 	"add r7, sp, #0xc\n\t"
 	"sub sp, #4\n\t"
-	"mov r3, #0\n\t" // ldr here chanded to this r3=0
+	"mov r3, #0\n\t"
 	);
 
 	asm (
@@ -52,7 +63,7 @@ __attribute ((naked, flatten)) void hook() {
 	);
 }
 
-unsigned int hook_armbx(void *addr, void* to) {
+static unsigned int hook_armbx(void *addr, void* to) {
 	((unsigned int   *) addr)[0] = 0xe92d0030;         // PUSH {R4-R5}
 	((unsigned int   *) addr)[1] = 0xe59f4004;         // LDR R4, =addr
 	((unsigned int   *) addr)[2] = 0xe1a0500f;         // MOV R5, PC
@@ -61,7 +72,7 @@ unsigned int hook_armbx(void *addr, void* to) {
 	return 5 * 4;
 }
 
-unsigned int hook_thumbbx(void *addr, void* to) {
+static unsigned int hook_thumbbx(void *addr, void* to) {
 	((unsigned short *) addr)[0] = 0xb430;         // PUSH {R4-R5}
 	((unsigned short *) addr)[1] = 0x4c01;         // LDR R4, =addr
 	((unsigned short *) addr)[2] = 0x467d;         // MOV R5, PC
@@ -74,14 +85,27 @@ void hook_thumb(void *addr, void* to) {
         unsigned int addrhook = (unsigned int) to;
         if (addrhook % 2 !=0) addrhook = addrhook - 1;
 	unsigned int hsize = (unsigned int) memfind4((void*) addrhook, 0x1000, 0xfeedface) - addrhook;
+	hsize += 0x100; // there's pool data after the payload
 	unsigned char* kbuf = (unsigned char*) kalloc(hsize);
         _memcpy(kbuf, (void*) addrhook, hsize);
-	hook_thumbbx(addr, (void *) (addrhook + 1));
+	hook_thumbbx(addr, (void *) (kbuf + 1));
+	//hook_thumbbx(addr, (void *) (addrhook + 1));
+}
+
+void hook_arm(void *addr, void* to) {
+        unsigned int addrhook = (unsigned int) to;
+        if (addrhook % 2 !=0) addrhook = addrhook - 1;
+	unsigned int hsize = (unsigned int) memfind4((void*) addrhook, 0x1000, 0xfeedface) - addrhook;
+	hsize += 0x100; // there's pool data after the payload
+	unsigned char* kbuf = (unsigned char*) kalloc(hsize);
+	_memcpy(kbuf, (void*) addrhook, hsize);
+	hook_armbx(addr, (void *) (kbuf + 1));
 }
 
 int patch_sandbox(void* address, unsigned int size) {
+	//hook_arm((void *) 0x803D1B94, hook);
 	hook_thumb((void *) 0x801DCCCC, hook);
-
-        kprintf("coucou\n");
+	IOLog("hey ho\n");
+	kprintf("heyho\n");
 }
 
